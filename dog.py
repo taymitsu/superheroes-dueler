d66e5f8c003056b2391ffd355652f843a2f7b931
Tay <tmitsumori96@gmail.com>
2021-11-01 20:51:59 -0700

    intro OOP completed and working

---
+++ dog.py
@@ -0,0 +1,9 @@
+class Dog: 
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+        print("dog initialized!")
+
+    def bork(self):
+        print("bork")
+
---
+++ my_dogs.py
@@ -0,0 +1,7 @@
+import dog
+
+my_dog = dog.Dog("Arty", "SuperDog")
+my_dog.bork()
+
+my_other_dog = dog.Dog("Tyson", "SuperDog")
+print(my_other_dog.name)
