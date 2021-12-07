89f06e0787072c66b2c0389c759e184f80e9d26e
Tay <tmitsumori96@gmail.com>
2021-12-06 21:04:46 -0800

    Added battle arena

--- hero.py
+++ hero.py
@@ -4,6 +4,119 @@ from armor import Armor
 from weapon import Weapon
 from team import Team
 
+class Arena:
+    def __init__(self):
+        self.team_one = Team("Team One")
+        self.team_two = Team("Team Two")
+    # TODO: create instance variables named team_one and team_two that
+    # will hold our teams.
+
+    def create_ability(self):
+        name = input("What is the ability name?  ")
+        max_damage = input("What is the max damage of the ability?  ")
+
+        return Ability(name, max_damage)
+
+    def create_weapon(self):
+        name = input("What is the name of this weapon?  ")
+        max_damage = input("What is the max damage of this weapon?  ")
+
+        return Weapon(name, max_damage)
+
+    def create_armor(self):
+        name = input("What is the armor name? > ")
+        max_block = input("What is the max block of the armor? > ")
+
+        return Armor(name, max_block)
+
+    def create_hero(self):
+        hero_name = input("Hero's name: ")
+        hero = Hero(hero_name)
+        add_item = None
+
+        while add_item != "4":
+            add_item = input("[1] Add ability\n[2] Add weapon\n[3] Add armor\n[4] Done adding items\n\nYour choice: ")
+            if add_item == "1":
+                hero.add_ability(self.create_ability())
+            elif add_item == "2":
+                hero.add_weapon(self.create_weapon())
+            elif add_item == "3":
+                hero.add_armor(self.create_armor())
+        return hero
+
+    def build_team_one(self):
+        numOfTeamMembers = int(input("How many members would you like on Team One?\n"))
+        for i in range(numOfTeamMembers):
+            hero = self.create_hero()
+            self.team_one.add_hero(hero)
+
+    def build_team_two(self):
+        numOfTeamMembers = int(input("How many members would you like on Team One?\n"))
+        for i in range(numOfTeamMembers):
+            hero = self.create_hero()
+            self.team_two.add_hero(hero)
+
+    def team_battle(self):
+        self.team_one.attack(self.team_two)
+
+    def show_stats(self):
+        print("\n")
+        print(self.team_one.name + " statistics: ")
+        self.team_one.stats()
+        print("\n")
+        print(self.team_two.name + " statistics: ")
+        self.team_two.stats()
+        print("\n")
+
+        team_kills = 0
+        team_deaths = 0
+        for hero in self.team_one.heroes:
+            team_kills += hero.kills
+            team_deaths += hero.deaths
+        if team_deaths == 0:
+            team_deaths = 1
+        print(self.team_one.name + " average K/D was: " + str(team_kills/team_deaths))
+
+        for hero in self.team_two.heroes:
+            team_kills += hero.kills
+            team_deaths += hero.deaths
+        if team_deaths == 0:
+            team_deaths = 1
+        print(self.team_two.name + " average K/D was: " + str(team_kills/team_deaths))
+
+        for hero in self.team_one.heroes:
+            if hero.deaths == 0:
+                print("survived from " + self.team_one.name + ": " + hero.name)
+
+        for hero in self.team_two.heroes:
+            if hero.deaths == 0:
+                print("survived from " + self.team_two.name + ": " + hero.name)
+
+    if __name__ == "__main__":
+        game_is_running = True
+
+        # Instantiate Game Arena
+        arena = Arena()
+
+        #Build Teams
+        arena.build_team_one()
+        arena.build_team_two()
+
+        while game_is_running:
+
+            arena.team_battle()
+            arena.show_stats()
+            play_again = input("Play Again? Y or N: ")
+
+            #Check for Player Input
+            if play_again.lower() == "n":
+                game_is_running = False
+
+            else:
+                #Revive heroes to play again
+                arena.team_one.revive_heroes()
+                arena.team_two.revive_heroes()
+
 class Hero:
     def __init__(self, name, starting_health=100):
         self.name = name
